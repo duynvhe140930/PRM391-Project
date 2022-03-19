00ac9b9d54ba5c802dbb08e536e8f7c153c3fb47
duynvhe140930 <duynvhe140930@fpt.edu.vn>
2022-03-19 09:16:01 +0700

    update

---
+++ app/build/generated/source/buildConfig/debug/com/is1427/is1427_g4_prm391_project/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.is1427.is1427_g4_prm391_project;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.is1427.se1427_g4_prm391_project";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
