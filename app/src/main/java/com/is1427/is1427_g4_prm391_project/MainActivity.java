24c53ae69d9bab11d6d601cda1a152702bc6c9ad
duynvhe140930 <duynvhe140930@fpt.edu.vn>
2022-03-19 17:37:52 +0700

    update source

--- app/src/main/java/com/is1427/is1427_g4_prm391_project/MainActivity.java
+++ app/src/main/java/com/is1427/is1427_g4_prm391_project/MainActivity.java
@@ -3,12 +3,34 @@ package com.is1427.is1427_g4_prm391_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText inputArea;
+    Button btnSearch, btnNextDay;
+    TextView txtDisplayCity, txtDisplayCountry, txtDisplayTemp, txtDisplayStatus, txtDisplayHumidity, txtDisplayCloud, txtDisplayWind, txtDisplayToday;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bindingView();
+    }
+
+    private void bindingView() {
+        inputArea = findViewById(R.id.inputArea);
+        btnSearch=findViewById(R.id.btnSearch);
+        btnNextDay=findViewById(R.id.btnNextDay);
+        txtDisplayCity=findViewById(R.id.txtDisplayCity);
+        txtDisplayCountry=findViewById(R.id.txtDisplayCountry);
+        txtDisplayTemp=findViewById(R.id.txtDisplayTemp);
+        txtDisplayStatus=findViewById(R.id.txtDisplayStatus);
+        txtDisplayHumidity=findViewById(R.id.txtDisplayHumidity);
+        txtDisplayCloud=findViewById(R.id.txtDisplayCloud);
+        txtDisplayWind=findViewById(R.id.txtDisplayWind);
+        txtDisplayToday=findViewById(R.id.txtDisplayToday);
     }
 }
